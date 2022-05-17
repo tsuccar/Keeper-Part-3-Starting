9ba76159be3193df0be19e18b29bceb8d0c029ec
tsuccar <tsuccar@users.noreply.github.com>
2022-05-17 15:31:49 -0400

    updated

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateArea() {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  function handleTitleChange(event) {
+    const newTitle = event.target.value;
+    console.log(newTitle);
+    setTitle(newTitle);
+  }
+  function handleBodyChange(event) {
+    const newBody = event.target.value;
+    console.log(newBody);
+    setBody(newBody);
+  }
+  function handleClick() {
+    console.log("Clicked");
+  }
+
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+      <form onClick={handleClick}>
+        <input onChange={handleTitleChange} name="title" placeholder="Title" />
+        <textarea
+          onChange={handleBodyChange}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+        />
         <button>Add</button>
       </form>
     </div>
